3e7ce41bcd22bbd222939761974d36cbe33ead21
unknown <dmitrii.website@gmail.com>
2020-08-02 16:51:32 +1200

    Typo fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react"
 import styled from "styled-components"
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 import { navigate, useStaticQuery, graphql } from 'gatsby'
 import { authenticateUser } from "../utils/userAuthenticate"
 
